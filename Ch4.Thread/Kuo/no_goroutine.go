2a25d96e1908aac100347e90f724d8a954ba70bd
Kuo SyuanWei <kuosyuanwei@team-lab.com>
2020-02-23 22:23:04 +0900

    fix error

--- Ch4.Thread/Kuo/no_goroutine.go
+++ Ch4.Thread/Kuo/no_goroutine.go
@@ -11,12 +11,12 @@ func withdraw1() {
 	time.Sleep(3000 * time.Millisecond)
 	balance -= 1000
 	money1 = balance
-	fmt.Println("After withdrawing $1000, balace: ", money)
+	fmt.Println("After withdrawing $1000, balace: ", money1)
 	wg1.Done()
 }
 
 var wg1 sync.WaitGroup
-var money1 int = 1500
+var money1 = 1500
 
 func main() {
 	fmt.Println("We have $1500")
