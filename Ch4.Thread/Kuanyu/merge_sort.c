bbae613873ed5dc7d3d0e0ad7864683808aab43f
kuanyu <kuanyu@localhost.localdomain>
2020-03-07 09:49:03 -0500

    merge_sort.c

--- Ch4.Thread/Kuanyu/merge_sort.c
+++ Ch4.Thread/Kuanyu/merge_sort.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <time.h>
+#include <sys/time.h>
 #include <pthread.h>
 
 typedef struct MergeSortParam {
@@ -36,16 +36,24 @@ s_MergeSortedArray(int *pnzSortingArray, int nBegin, int nMid, int nEnd)
 
     nzTmpArray = (int *)calloc(nEnd - nBegin + 1, sizeof(int));
 
-    for (l = nBegin, r = nMid + 1, i = 0; l <= nMid && r <= nEnd;) {
-        nzTmpArray[i] = pnzSortingArray[l] < pnzSortingArray[r] ? pnzSortingArray[l] : pnzSortingArray[r];
+    for (l = nBegin, r = nMid + 1, i = 0; l <= nMid && r <= nEnd;i++) {
+        if (pnzSortingArray[l] < pnzSortingArray[r]){
+            nzTmpArray[i] = pnzSortingArray[l];
+            l++;
+        } else {
+            nzTmpArray[i] = pnzSortingArray[r];
+            r++;
+        }
     }
     if (l <= nMid) memcpy(&nzTmpArray[i], &pnzSortingArray[l], sizeof(int) * (nMid - l + 1));
     else if (r <= nEnd) memcpy(&nzTmpArray[i], &pnzSortingArray[r], sizeof(int) * (nEnd - r + 1));
 
+    memcpy(&pnzSortingArray[nBegin], nzTmpArray, sizeof(int) * (nEnd - nBegin + 1));
+
     return 0;
 }
 
-static void
+static void*
 s_DoMergeSort(void *pParam)
 {
     MergeSortParam  *pstParam = (MergeSortParam *)pParam;
@@ -60,7 +68,7 @@ s_DoMergeSort(void *pParam)
     stParamToPassLeft.nBegin = pstParam->nBegin;
     stParamToPassLeft.nEnd = nMid;
     if (s_AllowCallThread()) {
-        pthread_create(&pid[0], NULL, s_DoMergeSort, (void *)stParamToPassLeft);
+        pthread_create(&pid[0], NULL, s_DoMergeSort, (void *)&stParamToPassLeft);
     } else {
         s_DoMergeSort((void *)&stParamToPassLeft);
     }
@@ -68,7 +76,7 @@ s_DoMergeSort(void *pParam)
     stParamToPassRight.nBegin = nMid + 1;
     stParamToPassRight.nEnd = pstParam->nEnd;
     if (s_AllowCallThread()) {
-        pthread_create(&pid[1], NULL, s_DoMergeSort, (void *)stParamToPassRight);
+        pthread_create(&pid[1], NULL, s_DoMergeSort, (void *)&stParamToPassRight);
     } else {
         s_DoMergeSort((void *)&stParamToPassRight);
     }
@@ -80,6 +88,8 @@ s_DoMergeSort(void *pParam)
     }
 
     s_MergeSortedArray(pstParam->pnArray, pstParam->nBegin, nMid, pstParam->nEnd);
+
+    return NULL;
 }
 
 int 
@@ -89,8 +99,9 @@ main(int argc, char *argv[])
     int *nzIntSeq = NULL;
     int i = 0, nTmp = 0;
     char *pcFilename = argv[1];
-    FILE *fp = NULL;
+    FILE *fp = NULL, *outputfp = NULL;
     MergeSortParam stParam;
+    struct timeval tStart, tEnd;
 
     nThreadNum = atoi(argv[2]);
     fp = fopen(pcFilename, "r");
@@ -114,17 +125,29 @@ main(int argc, char *argv[])
 
     stParam.pnArray = nzIntSeq;
     stParam.nBegin = 0;
-    stParam.nEnd = nIntNum;
+    stParam.nEnd = nIntNum - 1;
 
     if (pthread_mutex_init(&ThreadNumLock, NULL) != 0) {
         goto end;
     }
 
+    gettimeofday(&tStart, NULL);
     s_DoMergeSort((void *)&stParam);
+    gettimeofday(&tEnd, NULL);
+    printf("spend time: %ld ms\n", ((1000000 * (tEnd.tv_sec - tStart.tv_sec) + tEnd.tv_usec - tStart.tv_usec) / 1000));
+
+    outputfp = fopen("output", "w");
+    for (i = 0; i < nIntNum; i++) {
+        fprintf(outputfp, "%d\n", nzIntSeq[i]);
+    }
 end:
     if (fp != NULL) {
         fclose(fp);
         fp = NULL;
+    }
+    if (outputfp != NULL) {
+        fclose(outputfp);
+        outputfp = NULL;
     }
     if (nzIntSeq != NULL) {
         free(nzIntSeq);
