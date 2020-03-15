5a53f1b73ea7b08fa54bdd48b8b9f5aa7e5cab2f
unknown <justhit666@gmail.com>
2020-03-15 18:36:36 +0800

    sudoku  add function prototype and global variable

--- Ch4.Thread/Kuanyu/sudoku_validate.c
+++ Ch4.Thread/Kuanyu/sudoku_validate.c
@@ -1,96 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/time.h>
 #include <pthread.h>
 
-typedef struct MergeSortParam {
-    int *pnArray;
-    int nBegin;
-    int nEnd;
-} MergeSortParam;
+int nBlockToCheck = 0;
+pthread_mutex_t BlockToCheckLock;
+int SudokuArray[9 * 9] = {0};
 
-int nThreadNum = 0;
-pthread_mutex_t ThreadNumLock;
-
-static int s_AllowCallThread()
+static int s_GetBlockIndexToCheck()
 {
     int nRet = 0;
-    pthread_mutex_lock(&ThreadNumLock);
+    pthread_mutex_lock(&BlockToCheckLock);
 
-    if (nThreadNum > 0) {
-        nRet = 1;
-        nThreadNum--;
+    if (nBlockToCheck < 8) {
+        nRet = nBlockToCheck;
+        nBlockToCheck++;
     }
 
-    pthread_mutex_unlock(&ThreadNumLock);
+    pthread_mutex_unlock(&BlockToCheckLock);
 
     return nRet;
 }
 
-static int
-s_MergeSortedArray(int *pnzSortingArray, int nBegin, int nMid, int nEnd)
+static int s_CheckRow()
 {
-    int *nzTmpArray = NULL;
-    int i = 0, l = 0, r = 0;
-
-    nzTmpArray = (int *)calloc(nEnd - nBegin + 1, sizeof(int));
-
-    for (l = nBegin, r = nMid + 1, i = 0; l <= nMid && r <= nEnd;i++) {
-        if (pnzSortingArray[l] < pnzSortingArray[r]){
-            nzTmpArray[i] = pnzSortingArray[l];
-            l++;
-        } else {
-            nzTmpArray[i] = pnzSortingArray[r];
-            r++;
-        }
-    }
-    if (l <= nMid) memcpy(&nzTmpArray[i], &pnzSortingArray[l], sizeof(int) * (nMid - l + 1));
-    else if (r <= nEnd) memcpy(&nzTmpArray[i], &pnzSortingArray[r], sizeof(int) * (nEnd - r + 1));
-
-    memcpy(&pnzSortingArray[nBegin], nzTmpArray, sizeof(int) * (nEnd - nBegin + 1));
-
-    return 0;
 }
 
-static void*
-s_DoMergeSort(void *pParam)
+static int s_CheckColum()
 {
-    MergeSortParam  *pstParam = (MergeSortParam *)pParam;
-    MergeSortParam  stParamToPassLeft;
-    MergeSortParam  stParamToPassRight;
-    int nMid = (pstParam->nEnd + pstParam->nBegin) / 2;
-    pthread_t   pid[2] = {0};
-    if (pstParam->nEnd == pstParam->nBegin) {
-        return 0;
-    }
-    stParamToPassLeft.pnArray = pstParam->pnArray;
-    stParamToPassLeft.nBegin = pstParam->nBegin;
-    stParamToPassLeft.nEnd = nMid;
-    if (s_AllowCallThread()) {
-        pthread_create(&pid[0], NULL, s_DoMergeSort, (void *)&stParamToPassLeft);
-    } else {
-        s_DoMergeSort((void *)&stParamToPassLeft);
-    }
-    stParamToPassRight.pnArray = pstParam->pnArray;
-    stParamToPassRight.nBegin = nMid + 1;
-    stParamToPassRight.nEnd = pstParam->nEnd;
-    if (s_AllowCallThread()) {
-        pthread_create(&pid[1], NULL, s_DoMergeSort, (void *)&stParamToPassRight);
-    } else {
-        s_DoMergeSort((void *)&stParamToPassRight);
-    }
-    if (pid[0] > 0) {
-        pthread_join(pid[0], NULL);
-    }
-    if (pid[1] > 0) {
-        pthread_join(pid[1], NULL);
-    }
-
-    s_MergeSortedArray(pstParam->pnArray, pstParam->nBegin, nMid, pstParam->nEnd);
-
-    return NULL;
 }
+
+static int s_CheckBlock()
+{}
 
 int 
 main(int argc, char *argv[])
@@ -103,7 +44,6 @@ main(int argc, char *argv[])
     MergeSortParam stParam;
     struct timeval tStart, tEnd;
 
-    nThreadNum = atoi(argv[2]);
     fp = fopen(pcFilename, "r");
     if (fp == NULL) {
         goto end;
@@ -127,7 +67,7 @@ main(int argc, char *argv[])
     stParam.nBegin = 0;
     stParam.nEnd = nIntNum - 1;
 
-    if (pthread_mutex_init(&ThreadNumLock, NULL) != 0) {
+    if (pthread_mutex_init(&BlockToCheckLock, NULL) != 0) {
         goto end;
     }
 
@@ -153,5 +93,5 @@ end:
         free(nzIntSeq);
         nzIntSeq = NULL;
     }
-    pthread_mutex_destroy(&ThreadNumLock);
+    pthread_mutex_destroy(&BlockToCheckLock);
 }
