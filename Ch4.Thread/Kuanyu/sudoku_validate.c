8f9e6aafc369769a5388530b787b1f6fd6001b59
unknown <justhit666@gmail.com>
2020-03-15 18:36:36 +0800

    sudoku  add function prototype and global variable

--- Ch4.Thread/Kuanyu/sudoku_validate.c
+++ Ch4.Thread/Kuanyu/sudoku_validate.c
@@ -1,157 +1,147 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/time.h>
+#define _GNU_SOURCE
 #include <pthread.h>
+#include <signal.h>
+#include <sys/time.h>
+#include <ctype.h>
 
-typedef struct MergeSortParam {
-    int *pnArray;
-    int nBegin;
-    int nEnd;
-} MergeSortParam;
+int nBlockToCheck = 0;
+pthread_mutex_t BlockToCheckLock;
+int SudokuArray[9 * 9] = {0};
+int bValidSudoku = 1;
+pthread_t pid[3] = {0};
 
-int nThreadNum = 0;
-pthread_mutex_t ThreadNumLock;
-
-static int s_AllowCallThread()
+static int s_GetBlockIndexToCheck()
 {
     int nRet = 0;
-    pthread_mutex_lock(&ThreadNumLock);
+    pthread_mutex_lock(&BlockToCheckLock);
 
-    if (nThreadNum > 0) {
-        nRet = 1;
-        nThreadNum--;
+    if (nBlockToCheck < 8) {
+        nRet = nBlockToCheck;
+        nBlockToCheck++;
     }
 
-    pthread_mutex_unlock(&ThreadNumLock);
+    pthread_mutex_unlock(&BlockToCheckLock);
 
     return nRet;
 }
 
-static int
-s_MergeSortedArray(int *pnzSortingArray, int nBegin, int nMid, int nEnd)
+static void s_KillAllChildThread()
 {
-    int *nzTmpArray = NULL;
-    int i = 0, l = 0, r = 0;
+    int i = 0;
 
-    nzTmpArray = (int *)calloc(nEnd - nBegin + 1, sizeof(int));
-
-    for (l = nBegin, r = nMid + 1, i = 0; l <= nMid && r <= nEnd;i++) {
-        if (pnzSortingArray[l] < pnzSortingArray[r]){
-            nzTmpArray[i] = pnzSortingArray[l];
-            l++;
-        } else {
-            nzTmpArray[i] = pnzSortingArray[r];
-            r++;
+    for (i = 0; i < 3; i++) {
+        if (pid[i] > 0) {
+            pthread_kill(pid[i], SIGKILL);
+            pid[i] = 0;
         }
     }
-    if (l <= nMid) memcpy(&nzTmpArray[i], &pnzSortingArray[l], sizeof(int) * (nMid - l + 1));
-    else if (r <= nEnd) memcpy(&nzTmpArray[i], &pnzSortingArray[r], sizeof(int) * (nEnd - r + 1));
+}
 
-    memcpy(&pnzSortingArray[nBegin], nzTmpArray, sizeof(int) * (nEnd - nBegin + 1));
+static void* s_CheckRow(void* pvParam)
+{
+}
+
+static void* s_CheckColum(void* pvParam)
+{
+}
+
+static void* s_CheckBlock(void* pvParam)
+{
+}
+
+static int s_ValidateSudokuWithThread()
+{
+    int i = 0;
+    int nThreadRet = 0;
+
+    pthread_create(&pid[0], NULL, s_CheckRow, NULL);
+    pthread_create(&pid[1], NULL, s_CheckColum, NULL);
+    pthread_create(&pid[2], NULL, s_CheckBlock, NULL);
+
+    for (i = 0; i < 3; i = (i + 1) % 3) {
+        if (pthread_tryjoin_np(pid[i], NULL) == 0) {
+            if (!bValidSudoku) {
+                s_KillAllChildThread();
+                printf("Sudoku Invalid!\n");
+                break;
+            }
+        }
+    }
 
     return 0;
 }
 
-static void*
-s_DoMergeSort(void *pParam)
+static int s_ValidateSudoky()
 {
-    MergeSortParam  *pstParam = (MergeSortParam *)pParam;
-    MergeSortParam  stParamToPassLeft;
-    MergeSortParam  stParamToPassRight;
-    int nMid = (pstParam->nEnd + pstParam->nBegin) / 2;
-    pthread_t   pid[2] = {0};
-    if (pstParam->nEnd == pstParam->nBegin) {
-        return 0;
-    }
-    stParamToPassLeft.pnArray = pstParam->pnArray;
-    stParamToPassLeft.nBegin = pstParam->nBegin;
-    stParamToPassLeft.nEnd = nMid;
-    if (s_AllowCallThread()) {
-        pthread_create(&pid[0], NULL, s_DoMergeSort, (void *)&stParamToPassLeft);
-    } else {
-        s_DoMergeSort((void *)&stParamToPassLeft);
-    }
-    stParamToPassRight.pnArray = pstParam->pnArray;
-    stParamToPassRight.nBegin = nMid + 1;
-    stParamToPassRight.nEnd = pstParam->nEnd;
-    if (s_AllowCallThread()) {
-        pthread_create(&pid[1], NULL, s_DoMergeSort, (void *)&stParamToPassRight);
-    } else {
-        s_DoMergeSort((void *)&stParamToPassRight);
-    }
-    if (pid[0] > 0) {
-        pthread_join(pid[0], NULL);
-    }
-    if (pid[1] > 0) {
-        pthread_join(pid[1], NULL);
+    int nRet = 0;
+
+    s_CheckRow(NULL);
+    if (!bValidSudoku) {
+        nRet = -1;
+        goto end;
     }
 
-    s_MergeSortedArray(pstParam->pnArray, pstParam->nBegin, nMid, pstParam->nEnd);
+    s_CheckColum(NULL);
+    if (!bValidSudoku) {
+        nRet = -1;
+        goto end;
+    }
 
-    return NULL;
+    s_CheckBlock(NULL);
+    if (!bValidSudoku) {
+        nRet = -1;
+        goto end;
+    }
+
+end:
+    if (nRet != 0) {
+        printf("Sudoku invalid! single thread\n");
+    }
+    return 0;
 }
 
 int 
 main(int argc, char *argv[])
 {
-    int nIntNum = 0;
-    int *nzIntSeq = NULL;
-    int i = 0, nTmp = 0;
+    int i = 0;
+    char cTmp = '\0';
     char *pcFilename = argv[1];
-    FILE *fp = NULL, *outputfp = NULL;
-    MergeSortParam stParam;
+    FILE *fp = NULL;
     struct timeval tStart, tEnd;
 
-    nThreadNum = atoi(argv[2]);
     fp = fopen(pcFilename, "r");
     if (fp == NULL) {
         goto end;
     }
 
-    while (fscanf(fp, "%d", &nTmp) != EOF) {
-        i++;
+    while ((cTmp = fgetc(fp)) != EOF) {
+        if (isdigit(cTmp)) {
+           SudokuArray[i++] = cTmp - '0';
+        }
     }
-    nIntNum = i;
 
-    nzIntSeq = (int *)calloc(nIntNum, sizeof(int));
-    if (nzIntSeq == NULL) {
-        goto end;
+    for (i = 0;i < 9 * 9; i++) {
+        printf("%d ", SudokuArray[i]);
+        if (i % 9 == 8) {
+            printf("\n");
+        }
     }
-    rewind(fp);
 
-    i = 0;
-    while (fscanf(fp, "%d", &nzIntSeq[i++]) != EOF);
-
-    stParam.pnArray = nzIntSeq;
-    stParam.nBegin = 0;
-    stParam.nEnd = nIntNum - 1;
-
-    if (pthread_mutex_init(&ThreadNumLock, NULL) != 0) {
+    if (pthread_mutex_init(&BlockToCheckLock, NULL) != 0) {
         goto end;
     }
 
-    gettimeofday(&tStart, NULL);
-    s_DoMergeSort((void *)&stParam);
-    gettimeofday(&tEnd, NULL);
-    printf("spend time: %ld ms\n", ((1000000 * (tEnd.tv_sec - tStart.tv_sec) + tEnd.tv_usec - tStart.tv_usec) / 1000));
+//    gettimeofday(&tStart, NULL);
+//    gettimeofday(&tEnd, NULL);
+//    printf("spend time: %ld ms\n", ((1000000 * (tEnd.tv_sec - tStart.tv_sec) + tEnd.tv_usec - tStart.tv_usec) / 1000));
 
-    outputfp = fopen("output", "w");
-    for (i = 0; i < nIntNum; i++) {
-        fprintf(outputfp, "%d\n", nzIntSeq[i]);
-    }
 end:
     if (fp != NULL) {
         fclose(fp);
         fp = NULL;
     }
-    if (outputfp != NULL) {
-        fclose(outputfp);
-        outputfp = NULL;
-    }
-    if (nzIntSeq != NULL) {
-        free(nzIntSeq);
-        nzIntSeq = NULL;
-    }
-    pthread_mutex_destroy(&ThreadNumLock);
+    pthread_mutex_destroy(&BlockToCheckLock);
 }
